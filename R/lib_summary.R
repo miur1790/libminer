42c1b19d5894716ec92d1b0909eda5051f3034eb
miur1790 <miren.urrutiaiturritza@su.se>
2026-08-12 14:52:31 +0200

    Add MIT license and implement lib_summary() function

---
+++ R/lib_summary.R
@@ -0,0 +1,17 @@
+#' R Library Summary
+#'
+#' Provides a brief summary of the package libraries on your machine
+#'
+#' @returns A `data.frame` containing the count of packages in each of the
+#'   user's libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
+lib_summary <- function() {
+  pkgs <- utils::installed.packages()
+  pkg_tbl <- table(pkgs[, "LibPath"])
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+  names(pkg_df) <- c("Library", "n_packages")
+  pkg_df
+}
